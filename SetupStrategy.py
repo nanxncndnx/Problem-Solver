ebefe0b68d69388bd8ba544b1cfd4a5bbdf3cf65
nanxncndnx <machiavelli.co4@gmail.com>
2024-05-29 13:57:25 -0400

    just setup xla, float16 & distribution strategy for checking the number of GPU and if we dont have GPU we will use CPU

--- SetupStrategy.py
+++ SetupStrategy.py
@@ -17,3 +17,43 @@ def CheckGpu():
     print('TF version',tf.__version__)
     #well i will not using GPU but this is for you ===>
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU'))) # check GPU available
+
+#Lets setup xla, distribution strategy, float16 ==>
+def setup_strategy(xla, fp16, no_cuda):
+    print(" Tensorflow: setting up strategy")
+    
+    # setup xla
+    if xla:
+        print(" XLA Enabled")
+        tf.config.optimizer.set_jit(True)
+    
+    # setup mixed precision training
+    if fp16:
+        # Set to float16 at first
+        print(" Mixed Precision Training Enabled")
+        policy = tf.keras.mixed_precision.experimental.Policy("mixed_float16")
+        tf.keras.mixed_precision.experimental.set_policy(policy)
+    
+    # setup distribution strategy
+    gpus = tf.config.list_physical_devices("GPU")
+    if no_cuda:
+        strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
+    else:
+        if len(gpus) == 0:
+            print(" One Device Strategy [CPU] Enabled")
+            strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
+        elif len(gpus) == 1:
+            print(" One Device Strategy [GPU] Enabled")
+            strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
+        elif len(gpus) > 1:
+            print(" Mirrored Strategy Enabled")
+            # If only want to use a specific subset of GPUs use CUDA_VISIBLE_DEVICES=0`
+            strategy = tf.distribute.MirroredStrategy()
+        else:
+            strategy = tf.distribute.get_strategy()
+
+    return strategy
+
+def n_replicas(strategy):
+    # return number of devices
+    return strategy.num_replicas_in_sync
