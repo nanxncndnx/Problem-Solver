cb9174d8b7b7456a4020b08b96b70e68bac97744
nanxncndnx <machiavelli.co4@gmail.com>
2024-05-30 09:08:56 -0400

    just wanna changing the file name

--- mianLauncher.py
+++
@@ -1,8 +0,0 @@
-from SetupStrategy import CheckGpu, setup_strategy
-
-#first of all we will checking Tensorflow version and GPU ==>
-CheckGpu()
-
-# huggingface TF-T5 implementation has issues when mixed precision is enabled
-# we will disable FP16 for this but can be used for training any other model
-strategy = setup_strategy(xla=True, fp16=False, no_cuda=False)
